50a2896a4b8578783766dd514c6c372de6d509c2
ArnavJyo <arnav.jyotshi@gmail.com>
2024-03-19 16:57:48 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Explore from "./components/Explore";
 import SideBar from "./components/Sidebar";
 import SearchBar from "./components/TopHeader";
-import Info from "./components/Info";
+
 import Carousel from "./components/Carousel";
 import ExploreAiring from "./components/ExploreAiring";
 function App() {
